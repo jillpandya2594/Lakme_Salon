854e87b4d81bd57c1b71b0444c375145d4f08e88
jill pandya <jillpandya2594@gmail.com>
2023-04-11 19:09:41 +0530

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Lakme Salon</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!--Bootstrap-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" 
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
+      integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+      <!-- style css -->
+      <link rel="stylesheet" href="styles.css">
+      
+      
+      
+   </head>
+   <body>
+    <nav class="lakme-nav navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" >
+        <div class="top-logo-bar">
+            <div class="container px-5 py-3 d-flex">
+            <a class="navbar-brand" href="https://www.lakmesalon.in"> 
+               <img src="./images/Template-1.png" class="logo">
+                </a>
+                <ul class="navbar-nav ms-auto right-menu">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.lakmesalon.in/contact">
+                            Contact Us<span class="link-underline"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.lakmesalon.in/store-locator">
+                            Salon Locator <span class="link-underline"></span>
+                        </a>
+                    </li>
+                </ul>
+                <button class="navbar-toggler collapsed ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="animated-icon">
+                        <span class="navbar-toggler-icon-new"></span>
+                        <span class="navbar-toggler-icon-new"></span>
+                        <span class="navbar-toggler-icon-new"></span>
+                    </div>
+                </button>
+            </div>
+        </div>
+        <div class="container px-5 desk-menu">
+            <!-- <a class="navbar-brand" href="#page-top">Start Bootstrap</a> -->
+            
+                   
+    </nav>
+   </body>
+</html>
